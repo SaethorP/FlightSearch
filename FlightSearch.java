0e1371926613e73f8e859d65890258cc310b7484
sigurp <skoladoddi@gmail.com>
2016-03-03 15:17:05 +0000

    Bjó til nýjan klasa FlightSearch.java og setti allar get aðferðir úr FlightSearch.java inn í hann. Vann semsagt út frá nýju myndinni.

---
+++ FlightSearch.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class FlightSearch {
+	public void GetFlightById(int flightId) {}
+	public void GetFlightsById(int[] flightIds) {}
+	public void GetFlightsByCostumerId(int costumerId) {}
+	
+	public int[] GetFlightsIds(Date time ) { return new int[0]; }
+	public int[] GetFlightsIds(int price ) { return new int[0];}
+	public int[] GetFlightsIds(String destination ) { return new int[0]; }
+	
+	public int[] GetFlightsIds(Date time, int price ) { return new int[0]; }
+	public int[] GetFlightsIds(Date time, int price, String destination) { return new int[0]; }
+	public int[] GetFlightsIds(Date time, int price, String destination, Airport airport) { return new int[0]; }
+}
