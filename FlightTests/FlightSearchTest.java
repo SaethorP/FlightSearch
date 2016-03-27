f060a1e70573dc63a37558f743411f09fb6f9a2a
sigurp <skoladoddi@gmail.com>
2016-03-27 22:07:02 +0000

    Bættti við gildum sem þurfa að vera í upphafi, þarf eitthvað að lýta yfir það og bæta eftir þörfum.

--- FlightTests/FlightSearchTest.java
+++ FlightTests/FlightSearchTest.java
@@ -5,15 +5,35 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import Entities.Customer;
+import Entities.Airport;
+import Entities.Flight;
+import Entities.Plane;
 
 public class FlightSearchTest {
-
+	//Arrange
+	private Customer customer1;
+	private Airport airport1, airport2;
+	private Flight flight1;
+	private Plane plane1;
+	
 	@Before
 	public void setUp() throws Exception {
+		customer1 = new Customer();
+		airport1 = new Airport();
+		airport2 = new Airport();
+		flight1 = new Flight();
+		plane1 = new Plane();
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		customer1 = null;
+		airport1 = null;
+		airport2 = null;
+		flight1 = null;
+		plane1 = null;
 	}
 
 	@Test
