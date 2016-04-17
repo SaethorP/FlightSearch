7dbfe3958aa90917c33e16b8f0104fa9c32a7b05
sigurp <skoladoddi@gmail.com>
2016-04-17 21:50:54 +0000

    Breytti og einfaldaði Flight.java hlutnum.

--- Entities/Flight.java
+++ Entities/Flight.java
@@ -5,78 +5,46 @@ import java.util.Date;
 public class Flight {
 	
 	//Variables that start with _varible are private variables. 
-	private String _flightId;
-	private Date _arrive;
 	private Date _departure;
-	private Plane _plane;
-	private Airport[] _airports;
 	private String _to; 
 	private String _from;
 	private double _price;
-	private int[] _customers;
-	private Airport airport1, airport2;
+	private int _emptyseats;
 	
 	//Default constructor
 	public Flight(){
-		airport1 = new Airport();
-		airport2 = new Airport();
-		_airports = new Airport[2];
-		_airports[0] = airport1;
-		_airports[1] = airport2;
-		
-		_flightId = "123"; 
-		_arrive = new Date(); 
-		_departure = new Date(); 
-		_plane = new Plane();  
+ 
+		_departure = new Date();   
 		_to = "Akureyri";
 		_from = "Reykjavik";
-		_customers = new int[2];
-		_customers[0] = 1;
-		_customers[2] = 2;
-		
 		_price = 100;
+		_emptyseats = 10;
+		
 	}
 	
 	
-	public Flight(String flightId, Date arrive, Date departure, Plane plane, Airport[] airports, int[] customers, double price)
+	public Flight(Date departure, String to, String from, int emptyseats, double price)
 	{
-		_flightId = flightId; 
-		_arrive = arrive; 
-		_departure = departure; 
-		_plane = plane; 
-		_airports = airports; 
-		_to = airports[1].GetAirportCity();
-		_from = airports[0].GetAirportCity();
-		_customers = customers;
+		_departure = departure;  
+		_to = to;
+		_from = from;
+		_emptyseats = emptyseats;
 		_price = price;
 	}
 	
 	// Getters and setters for flight.
 	
-	public String getFlightId(){	return _flightId; }
-	public Date getArriveDate(){	return _arrive; }
 	public Date getDepartureDate(){ return _departure; }
-	public Plane getPlane(){ return _plane; }
-	public Airport[] getAirports(){ return _airports; }
 	public String getWhereTo(){ return _to; }
 	public String getWhereFrom(){ return _from; }
-	public int[] GetCustomers(){ return _customers; }
 	public double GetPrice(){ return _price; }
+	public int getemptyseats(){return _emptyseats;}
 	
-	public void setFlightId(String flightId){ _flightId = flightId; }
-	public void setArriveDate(Date arrive){_arrive = arrive;}
 	public void setDepartureDate(Date departure){ _departure = departure; }
-	public void setPlane(Plane plane){ _plane = plane; }
-	public void setAirports(Airport[] airports){ _airports = airports; }
 	public void setWhereTo(String to){ _to = to; }
 	public void setWhereFrom(String from){ _from = from;}
-	public void setCostumers(int[] customers){ _customers = customers;}
 	public void setPrice(double price){ _price = price;}
-	
-	public void EditPrice(int price)
-	{
-		
-	}
+	public void setEmptyseats(int emptyseats){ _emptyseats = emptyseats;}
 	
 	//Testing area for the class. 
 	public static void main(String[] args){
@@ -86,5 +54,4 @@ public class Flight {
 		
 	}
 	
-
 }
